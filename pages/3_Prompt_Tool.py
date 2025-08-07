c6f7d1349305f96103397a3012d7abcc2c6f1faa
Neo-Nguyen-95 <dzung.usth@gmail.com>
2025-08-07 18:25:51 +0700

    Update 3_Prompt_Tool.py

--- pages/3_Prompt_Tool.py
+++ pages/3_Prompt_Tool.py
@@ -1,11 +1,11 @@
-#%% LIB
+#%% I. LIB
 import streamlit as st
 from data import SQLRepository
 import os
 import sqlite3
 from pathlib import Path
 
-#%% DATA
+#%% II. DATA
 
 BASE_DIR = Path(__file__).resolve().parent.parent  # Access base dir in streamlit
 database_name = str(list(BASE_DIR.glob('*.db'))[0])
@@ -16,7 +16,7 @@ connection = sqlite3.connect(
     )
 repo = SQLRepository(connection)
 
-#%% APP
+#%% III. APP
 st.markdown("""
 # PROMPT TOOL
 
@@ -25,10 +25,10 @@ Thầy cô nhập các thông tin theo từng bước bên dưới.
 --- 
 """)
 
-    #%%% USER INPUT
+    #%%% 3.1 USER INPUT
 col1, col2 = st.columns(2)
 with col1:
-    #----------------------------------------------------------
+        #%%%% 3.1.1 CONTEXT
     st.markdown("""
     ### I. BỐI CẢNH
                 """)
@@ -40,7 +40,14 @@ with col1:
         "Chọn lớp", 
         repo.get_grade_list()
         )
-    book = st.selectbox(
+    if int(grade) in range(1, 6):
+        school = 'tiểu học'
+    elif int(grade) in range(6, 10):
+        school = 'trung học cơ sở'
+    else:
+        school = 'trung học phổ thông'
+    
+    author = st.selectbox(
         "Chọn bộ sách",
         repo.get_author_list()
         )
@@ -64,7 +71,7 @@ with col1:
             ) 
      
 with col2:
-    #----------------------------------------------------------
+        #%%%% 3.1.2 OBJECTIVE
     st.markdown("""
     ### II. NHIỆM VỤ
                 """)
@@ -91,17 +98,17 @@ st.markdown("""
         
 col1, col2 = st.columns(2)
 with col1:
-    #----------------------------------------------------------
+        #%%%% 3.1.3 DATA
     st.markdown("""
     ### III. DỮ LIỆU
                 """)
     
-    st.multiselect(
+    lesson_list = st.multiselect(
         "Chọn các bài học",
-        ["blank list"]
+        repo.get_lesson_list(subject, grade, author)
         )
     
-    # st.write(f"Dữ liệu {subject} {grade} - bộ sách {book} sẽ được nhập tự động!")
+    # st.write(f"Dữ liệu {subject} {grade} - bộ sách {author} sẽ được nhập tự động!")
     try:
         if khbd != "theo mẫu riêng":
             st.write(f"KHBD {khbd} sẽ được nhập tự động!")
@@ -110,7 +117,7 @@ with col1:
     
     
 with col2:
-    #----------------------------------------------------------
+        #%%%% 3.1.4 RESULT
     st.markdown("""
     ### IV. KẾT QUẢ
                 """)
@@ -123,17 +130,17 @@ with col2:
         st.text_area("Nhập các bước AI cần thực hiện tại")
     
 
-    #%%% PROMPT OUTPUT
+    #%%% 3.2 PROMPT OUTPUT
 st.markdown("""
 ---
 ### Kết quả Prompt
             """)
             
-#----------------------------------------------------------
+        #%%%% 3.2.1 CONTEXT OUTPUT
             
 result_prompt =  (
     "#BỐI CẢNH" + 
-    f"\nBạn là giáo viên {subject} {grade}. Bạn sử dụng bộ sách {book}. "
+    f"\nBạn là giáo viên {subject} {grade}. Bạn sử dụng bộ sách {author}. Bạn dạy học theo chương trình giáo dục phổ thông 2018, ở cấp {school}."
     )
 
 if need_teaching_technique:
@@ -152,7 +159,7 @@ if need_more_context:
 
 
 
-#----------------------------------------------------------
+        #%%%% 3.2.2 OBJECTIVE OUTPUT
 result_prompt +=  (
     "\n\n#NHIỆM VỤ" +
     f"\nNhiệm vụ của bạn là {task.lower()}."
@@ -168,22 +175,23 @@ result_prompt +=  (
 
 
 
-#----------------------------------------------------------
+        #%%%% 3.2.3 DATA OUTPUT
 result_prompt +=  (
     "\n\n#DỮ LIỆU\n" 
     
     )
 
+try:
+    for lesson in lesson_list:
+        result_prompt += f"##Bài {lesson}:\n {repo.get_knowledge(subject, grade, author, lesson)}"
+    
+except:
+    pass
 
 
 
 
-
-
-
-
-
-#----------------------------------------------------------
+        #%%%% 3.2.4 RESULT OUTPUT
 result_prompt +=  (
     "\n\n#KẾT QUẢ\n" 
     
@@ -191,9 +199,11 @@ result_prompt +=  (
 
 try:
     if khbd == "theo công văn 5512/BGD":
-        ...
+        result_prompt += f"Trả kết quả theo mẫu sau:\n{repo.get_cong_van('5512/BGDĐT-GDTrH', 'khung khbd')}"
+    elif khbd == "theo công văn 2345/BGD":
+        result_prompt += f"Trả kết quả theo mẫu sau:\n{repo.get_cong_van('2345/BGDĐT-GDTH', 'khung khbd')}"
 except:
-    ...
+    pass
 
 st.text_area(
     label="Prompt:",
